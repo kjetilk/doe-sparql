70610dc31d12bd69bb59ffb51238bd81e944f8c5
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2013-05-31 15:01:53 +0200

    Use more sensible names for the different 4store kbs

--- run.sh
+++ run.sh
@@ -10,16 +10,16 @@ fi
 
 killall 4s-backend 
 killall 4s-httpd
-cd 4store-unmodified/
-bin/4s-backend $largename"unmod"
-bin/4s-httpd -s -1 -p 8022 $largename"unmod"
-bin/4s-backend smallunmod
-bin/4s-httpd -s -1 -p 8021 smallunmod
-cd ../4store-sleep/
-bin/4s-backend $largename"sleep"
-bin/4s-backend smallsleep
-bin/4s-httpd -s -1 -p 8012 $largename"sleep"
-bin/4s-httpd -s -1 -p 8011 smallsleep
+cd 4store-filter-slow/
+bin/4s-backend $largename"filter"
+bin/4s-httpd -s -1 -p 8022 $largename"filter"
+bin/4s-backend smallfilter
+bin/4s-httpd -s -1 -p 8021 smallfilter
+cd ../4store-bgp-slow/
+bin/4s-backend $largename"bgp"
+bin/4s-backend smallbgp
+bin/4s-httpd -s -1 -p 8012 $largename"bgp"
+bin/4s-httpd -s -1 -p 8011 smallbgp
 ps ux | grep 4s
 
 curl -s -o /dev/null -w "%{url_effective} %{http_code}\n" http://localhost:8011/status/
